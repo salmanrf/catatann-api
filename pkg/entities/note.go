cdf1c5f8271c99bede4fefb01c5b47d9aa31ee9a
salmanrf <salmanrf2@gmail.com>
2023-01-22 18:12:38 +0700

    note use uuid as primary key

--- pkg/entities/note.go
+++ pkg/entities/note.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Note struct {
   gorm.Model
+	NoteId string `json:"note_id" gorm:"field:note_id;type:uuid;primaryKey;default:gen_random_uuid();"`
   Title string `gorm:"type:varchar(255);not null;"`
   Content string `gorm:"type:text;not null;"`
 }
