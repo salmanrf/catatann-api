fb63a4b1a3d543a8591913bdf16030250ff87844
salmanrf <salmanrf2@gmail.com>
2023-03-17 18:22:35 +0700

    refactor: use env variable for cors origin

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -2,16 +2,13 @@ package middlewares
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Cors(c * fiber.Ctx) error {
-	// fmt.Println("cookies", c.Cookies("ctnn_access_token"))
-	// fmt.Println("origin", c.GetReqHeaders()["Origin"])
-	// fmt.Println("METHOD", c.Method())
-	
-	c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	c.Set("Access-Control-Allow-Origin", os.Getenv("CATATANN_CLIENT_URL"))
 	c.Set("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept")
 	c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
 	c.Set("Access-Control-Allow-Credentials", "true")
